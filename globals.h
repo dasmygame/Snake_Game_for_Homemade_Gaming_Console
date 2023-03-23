3781140e5afa99463811d8555f5c1bbb5c967b37
Rahul Das <48290252+dasmygame@users.noreply.github.com>
2023-03-23 16:43:43 -0400

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,11 +1,5 @@
 // ============================================
 // The header file for general project settings
-//
-// Copyright 2022 Georgia Tech. All rights reserved.
-// The materials provided by the instructor in this course are for
-// the use of the students currently enrolled in the course.
-// Copyrighted course materials may not be further disseminated.
-// This file must NOT be made publicly available anywhere.
 //==================================================================
 
 
